4d96dfe3bcdc55248ce41551cef88bdea79236d8
Nazim6269 <nazimdev10022001@gmail.com>
2026-01-10 09:00:00 +0600

    Update MongoDB cluster connection

--- config/dbConnect.mjs
+++ config/dbConnect.mjs
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 const connectDB = async () => {
   try {
     await mongoose.connect(
-      `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.t42mo3d.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+      `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.aujbmom.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
     );
     console.log('Connected to MongoDB');
   } catch (error) {
